a3626e3a917473de776e3ff44c1803325d1dc740
leejeyoung <bulljy@naver.com>
2022-05-08 01:52:23 +0900

    prettier

--- section15_custom_hook/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
+++ section15_custom_hook/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -1,42 +1,40 @@
 import { useState } from "react";
 
 const useHttp = (requestConfig, applyData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const sendRequest = async () => {
-        setIsLoading(true);
-        setError(null);
-        try {
-            const response = await fetch(
-                'https://react-http-6b4a6.firebaseio.com/tasks.json', {
-                url:
-                }
-            );
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-8963a-default-rtdb.firebaseio.com/tasks.json"
+      );
 
-            if (!response.ok) {
-                throw new Error('Request failed!');
-            }
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
 
-            const data = await response.json();
+      const data = await response.json();
 
-            const loadedTasks = [];
+      const loadedTasks = [];
 
-            for (const taskKey in data) {
-                loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-            }
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
 
-            applyData(loadedTasks);
-        } catch (err) {
-            setError(err.message || 'Something went wrong!');
-        }
-        setIsLoading(false);
-    };
-
-    return {
-        isLoading,
-        error,
-        sendRequest,
+      applyData(loadedTasks);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
     }
-}
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
 
 export default useHttp;
