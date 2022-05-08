71e133296249ff56f9d594ebc8f25200e410adeb
leejeyoung <bulljy@naver.com>
2022-05-09 01:04:48 +0900

    section 15 complete

--- section15_custom_hook/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
+++ section15_custom_hook/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -1,34 +1,30 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const sendRequest = async () => {
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        "https://react-http-8963a-default-rtdb.firebaseio.com/tasks.json"
-      );
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
       if (!response.ok) {
         throw new Error("Request failed!");
       }
 
       const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      applyData(loadedTasks);
+      applyData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
+  }, []);
 
   return {
     isLoading,
